cd045302649aa366a6fcc51394d2e2b49ca4a569
Dongmin Yu <ydm2790@gmail.com>
2024-10-28 00:51:50 +0900

    chore: 🏷️ add `UserInput` type default documentation comment

--- types/user-input.ts
+++ types/user-input.ts
@@ -14,12 +14,24 @@ export interface UserInput {
   jobGroupId?: JobGroupId;
   /** 채용 직무 카테고리 */
   jobIds: JobIds[];
-  /** 채용 경력 */
+  /**
+   * 채용 경력
+   * @default '-1'
+   */
   years: Years[];
-  /** 국가 코드 */
+  /**
+   * 국가 코드
+   * @default 'kr'
+   */
   countryKey?: CountryCode;
-  /** 지역 코드 */
+  /**
+   * 지역 코드
+   * @default 'all'
+   */
   locationKey: string;
-  /** 결과 개수 제한 */
+  /**
+   * 결과 개수 제한
+   * @default 20
+   */
   limit?: number;
 }

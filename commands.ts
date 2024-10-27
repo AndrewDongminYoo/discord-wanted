f32e8fdfca5848eac1f029c6282b10d4f84e6078
Dongmin Yu <ydm2790@gmail.com>
2024-10-27 21:00:14 +0900

    feat: ✨ adding Wanted Parameter Options to Actual Use in Discord

--- commands.ts
+++ commands.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import { type Commands, InstallGlobalCommands } from './utils.js';
 
 const APPLICATION_ID = process.env.APPLICATION_ID;
 console.debug('🚀 - APPLICATION_ID:', APPLICATION_ID);
@@ -10,41 +9,83 @@ if (!APPLICATION_ID) {
   throw new Error('APPLICATION_ID is not defined in environment variables.');
 }
 
-// game.js에서 게임 선택 사항 가져오기
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (const choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// 간단한 테스트 명령
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
-
-// 옵션이 포함된 명령
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
+// 원티드 크롤링 옵션
+const WANTED_COMMAND: Commands = {
+  name: 'wanted',
+  description: '채용 정보를 검색합니다.',
   options: [
     {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
+      name: 'location',
+      description: '채용 지역을 선택하세요',
+      type: 3, // STRING
       required: true,
-      choices: createCommandChoices(),
+      choices: [
+        { name: 'all', value: '전국' },
+        { name: 'seoul', value: '서울' },
+        { name: 'busan', value: '부산' },
+        { name: 'daegu', value: '대구' },
+        { name: 'incheon', value: '인천' },
+        { name: 'gwangju', value: '광주' },
+        { name: 'daejeon', value: '대전' },
+        { name: 'ulsan', value: '울산' },
+        { name: 'sejong', value: '세종' },
+        { name: 'gyeonggi', value: '경기' },
+        { name: 'gangwon', value: '강원' },
+        { name: 'n-chungcheong', value: '충북' },
+        { name: 's-chungcheong', value: '충남' },
+        { name: 'n-jeolla', value: '전북' },
+        { name: 's-jeolla', value: '전남' },
+        { name: 'n-gyeongsang', value: '경북' },
+        { name: 's-gyeongsang', value: '경남' },
+        { name: 'jeju', value: '제주' },
+      ],
+    },
+    {
+      name: 'years',
+      description: '경력 조건을 입력하세요',
+      type: 3, // STRING
+      required: true,
+      choices: [
+        { name: 'all', value: '-1' },
+        { name: 'new', value: '0' },
+        { name: '1 year', value: '1' },
+        { name: '2 years', value: '2' },
+        { name: '3 years', value: '3' },
+        { name: '4 years', value: '4' },
+        { name: '5 years', value: '5' },
+        { name: '6 years', value: '6' },
+        { name: '7 years', value: '7' },
+        { name: '8 years', value: '8' },
+        { name: '9 years', value: '9' },
+        { name: '10 years', value: '10' },
+      ],
+    },
+    {
+      name: 'job-id',
+      description: '직무를 입력하세요',
+      type: 4, // INTEGER
+      required: true,
+      choices: [
+        { name: 'Software Engineer', value: 10110 },
+        { name: 'Web Developer', value: 873 },
+        { name: 'Server Developer', value: 872 },
+        { name: 'Frontend Developer', value: 669 },
+        { name: 'Java Developer', value: 660 },
+        { name: 'C Developer', value: 900 },
+        { name: 'Python Developer', value: 899 },
+        { name: 'Ml Engineer', value: 1634 },
+        { name: 'DevOps Engineer', value: 674 },
+        { name: 'Nodejs Developer', value: 895 },
+        { name: 'Android Developer', value: 677 },
+        { name: 'iOS Developer', value: 678 },
+        { name: 'Data Scientist', value: 1024 },
+        { name: 'Security Engineer', value: 671 },
+        { name: 'Big Data Engineer', value: 1025 },
+        { name: 'Blockchain Engineer', value: 1027 },
+        { name: 'Cross Platform Developer', value: 10111 },
+        { name: 'PHP Developer', value: 893 },
+        { name: 'Ruby on Rails Developer', value: 894 },
+      ],
     },
   ],
   type: 1,
@@ -52,6 +93,6 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [WANTED_COMMAND];
 
 InstallGlobalCommands(APPLICATION_ID, ALL_COMMANDS);
